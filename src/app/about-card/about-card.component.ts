cd51fe2f4875d3af722937841c1feb5a982e0f6f
RasmusJuelN <rasmusn686@hotmail.com>
2024-02-21 13:46:34 +0100

    about-cards done

--- src/app/about-card/about-card.component.ts
+++ src/app/about-card/about-card.component.ts
@@ -1,16 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'; // Import OnInit
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-about-card',
   templateUrl: './about-card.component.html',
   styleUrls: ['./about-card.component.scss']
 })
-export class AboutCardComponent {
+export class AboutCardComponent { // Implement OnInit
 
-  @Input() icon: any | undefined;
+  constructor(private sanitizer: DomSanitizer) { }
+  getSafeSvg(svgContent: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(svgContent);
+  }
+  @Input() svgContent: string = '';
   @Input() title: string ="";
   @Input() text: any | undefined;
+  @Input() isEven: boolean = false;
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import AOS from 'aos';
 import { AppModule } from './app/app.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+.then(() => {
+  AOS.init();
+})
   .catch(err => console.error(err));
