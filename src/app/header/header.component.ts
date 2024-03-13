5abf89f66b56c8d66a21bfb8bf1d39a121520b7f
RasmusJuelN <rasmusn686@hotmail.com>
2024-03-13 13:57:58 +0100

    footer contact

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component , Input} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,24 @@ import { Component , Input} from '@angular/core';
 })
 export class HeaderComponent {
   @Input() heading: string = 'Online CV'
+
+  constructor(private router: Router) { }
+
+  navigateToContact() {
+    // navigate to a dummy fragment
+    this.router.navigate(['/home'], { fragment: 'd' }).then(() => {
+      // then navigate to the actual fragment
+      this.router.navigate(['/home'], { fragment: 'contact' });
+    });
+  }
+
+  navigateToHome() {
+    // navigate to a dummy fragment
+    this.router.navigate(['/home'], { fragment: 'd' }).then(() => {
+      // then navigate to the actual fragment
+      this.router.navigate(['/home'], { fragment: 'home' });
+    });
+  }
+
 }
+
